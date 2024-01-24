bd12025c8ac2ba03387639cf41f07163886a89e3
Edanur <ednrcnr2412@gmail.com>
2024-01-25 00:57:52 +0300

    commitlendii

--- Desktop/teamodev/okuma.py
+++ Desktop/teamodev/okuma.py
@@ -1,59 +1,32 @@
 import socket
 import struct
-import time
 
-def send_message_buffer(client_socket, message, buffer_size):
-    message_bytes = message.encode('utf-8')
-    message_size_bytes = struct.pack("!I", len(message_bytes))
-    client_socket.sendall(message_size_bytes)
 
-    offset = 0
-    while offset < len(message_bytes):
-        end = offset + buffer_size
-        chunk = message_bytes[offset:end]
-        client_socket.sendall(chunk)
-        offset = end
+def int_to_bytearray(value):
+    return struct.pack('>I', value)
 
-def receive_message_buffer(client_socket, buffer_size):
-    message_size_bytes = client_socket.recv(4)
-    message_size = struct.unpack("!I", message_size_bytes)[0]
 
-    received_message = b""
-    while len(received_message) < message_size:
-        chunk = client_socket.recv(buffer_size)
-        received_message += chunk
+def string_to_bytearray(value, length=32):
+    return value.encode('utf-8').ljust(length, b'\0')
 
-    return received_message.decode('utf-8')
 
-server_ip = "34.234.82.161"
-server_port = 13571
+class Connection:
+    def __init__(self, conn_name, topic_name, buffer_size=1024):
+        self.conn_name = conn_name
+        self.topic_name = topic_name
+        self.buffer_size = buffer_size
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(("localhost", 13571))
+        self.socket.sendall(string_to_bytearray(conn_name))
+        self.socket.sendall(string_to_bytearray(topic_name))
+        self.socket.sendall(int_to_bytearray(buffer_size))
 
-try:
-    client_socket.connect((server_ip, server_port))
-    print("sunucuya bağlanıldı...")
-    connection_name = "Edanur"
-    topic_name = "ödev"
-    message = "message"
-    buffer_size = 16
+    def read(self):
+        return self.socket.recv(self.buffer_size)
 
-    send_message_buffer(client_socket, connection_name, buffer_size)
-    send_message_buffer(client_socket, topic_name, buffer_size)
-    send_message_buffer(client_socket, message, buffer_size)
+    def send(self, message_bytes):
+        self.socket.sendall(message_bytes)
 
-    print("mesaj gönderildi.")
-    time.sleep(5)
-    received_connection_name = receive_message_buffer(client_socket, buffer_size)
-    received_topic_name = receive_message_buffer(client_socket, buffer_size)
-    received_message = receive_message_buffer(client_socket, buffer_size)
-    print(received_connection_name)
-    print(received_topic_name)
-    print(received_message)
-
-except Exception as e:
-    print("hata oluştu:", e)
-finally:
-    client_socket.close()
-    print("bağlantı kapatıldı.")
-    
+    def close(self):
+        self.socket.close()
--- Desktop/teamodev/tcp.py
+++ Desktop/teamodev/tcp.py
@@ -1,55 +1,63 @@
 import socket
 import struct
 import time
+import threading
 
-def send_message_buffer(client_socket, message, buffer_size):
-    message_bytes = message.encode('utf-8')
-    message_size_bytes = struct.pack("!I", len(message_bytes))
-    client_socket.sendall(message_size_bytes)
-
-    offset = 0
-    while offset < len(message_bytes):
-        end = offset + buffer_size
-        chunk = message_bytes[offset:end]
-        client_socket.sendall(chunk)
-        offset = end
-
-def receive_message_buffer(client_socket, buffer_size):
-    message_size_bytes = client_socket.recv(4)
-    message_size = struct.unpack("!I", message_size_bytes)[0]
-
-    received_message = b""
-    while len(received_message) < message_size:
-        chunk = client_socket.recv(buffer_size)
-        received_message += chunk
-
-    return received_message.decode('utf-8')
+   
+    
 
 server_ip = "34.234.82.161"
 server_port = 13571
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def listen():
+    while True:
+        data=client_socket.recv(1024)
+        print(data)
+    
+
+# buffer_size'ın string olarak uzunluğunu kontrol et
+
 
 try:
     client_socket.connect((server_ip, server_port))
     print("sunucuya bağlanıldı...")
     connection_name = "Edanur"
-    topic_name = "ödev"
+    connection_array=connection_name.encode('utf-8')
+    #(69,64,61,110,117,114)
+    if len(connection_array) < 32:
+        padding_size = 32 - len(connection_array)
+        padding = b'\x00' * padding_size
+        connection_array += padding
+
+    topic_name = "test_topic"
+    topic_array=topic_name.encode('utf-8')
+    if len(topic_array)<32:
+        padding_size=32 - len(topic_array)
+        padding=b'\x00' *padding_size
+        topic_array +=padding
     message = "message"
-    buffer_size = 16
 
-    send_message_buffer(client_socket, connection_name, buffer_size)
-    send_message_buffer(client_socket, topic_name, buffer_size)
-    send_message_buffer(client_socket, message, buffer_size)
+    buffer_size = struct.pack('!I', 1024)
+    client_socket.sendall(connection_array)
+    client_socket.sendall(topic_array)
+    client_socket.sendall(buffer_size)
+    threading.Thread(target=listen, args=()).start()
+    while True:
+        kullanıcı=input("mesaj:")
+        kullanıcı_array=kullanıcı.encode('utf-8')
+        if len(kullanıcı)<1024:
+            padding_size=1024-len(kullanıcı_array)
+            padding=b'\x00' *padding_size
+            kullanıcı_array +=padding
+        client_socket.sendall(kullanıcı_array)
+    
+            
 
+    
     print("mesaj gönderildi.")
     time.sleep(5)
-    received_connection_name = receive_message_buffer(client_socket, buffer_size)
-    received_topic_name = receive_message_buffer(client_socket, buffer_size)
-    received_message = receive_message_buffer(client_socket, buffer_size)
-    print(received_connection_name)
-    print(received_topic_name)
-    print(received_message)
+    
 
 except Exception as e:
     print("hata oluştu:", e)
